3fa33ef4ce8e6cc87d273a2635875519e61e5fc1
UMEDJAN KABIRI <umedjan.kabiri@gmail.com>
2023-09-29 17:40:28 +0300

    changes

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,4 +1,4 @@
-let number1 = Math.floor(Math.random() * 100);
-number1 % 2 === 0
-  ? console.log(number1, "Это четное число")
-  : console.log(number1, "Это нечетное число");
+// let number1 = Math.floor(Math.random() * 100);
+// number1 % 2 === 0
+//   ? console.log(number1, "Это четное число")
+//   : console.log(number1, "Это нечетное число");
