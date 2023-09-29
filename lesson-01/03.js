874d3bbd8f2ad39ae4f26336785653a6ed81efed
UMEDJAN KABIRI <umedjan.kabiri@gmail.com>
2023-09-29 14:42:17 +0300

    changes in file

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -6,6 +6,6 @@
 //  С каждой перезагрузкой страницы у тебя будет новое число.​
 
 let number1 = Math.floor(Math.random() * 100);
-number1 % 2
-  ? console.log(number1, "четное число")
-  : console.log(number1, "нечетное число");
+number1 % 2 === 0
+  ? console.log(number1, "Это четное число")
+  : console.log(number1, "Это нечетное число");
