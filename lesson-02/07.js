f79dd3800b6afc3cd8e8a8d333ba20e4ecf61751
UMEDJAN KABIRI <umedjan.kabiri@gmail.com>
2023-10-02 01:48:24 +0300

    changes

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -7,7 +7,7 @@ function Married() {
       city: "Bobryisk",
     },
   };
-  let passportMarried2 = { married: true, ...passportMarried };
+  let passportMarried2 = { ...passportMarried };
   passportMarried2.married = true;
   console.log(passportMarried);
   console.log(passportMarried2);
