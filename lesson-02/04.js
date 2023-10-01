97aa300dd78c323ddb49a2e66d4439f9f094403e
UMEDJAN KABIRI <umedjan.kabiri@gmail.com>
2023-10-01 21:02:41 +0300

    changes

---
+++ lesson-02/04.js
@@ -0,0 +1,8 @@
+const giveMeRightCondition = (value1, value2, maximum) => {
+  if (value1 === value2 && value1 <= maximum) {
+    console.log("Success!");
+  } else {
+    console.log("Something wrong!");
+  }
+};
+giveMeRightCondition(100, 99, 200);
