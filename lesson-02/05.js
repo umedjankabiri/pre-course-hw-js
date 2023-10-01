ade571bea204f4d956690fb8500144f48576c620
UMEDJAN KABIRI <umedjan.kabiri@gmail.com>
2023-10-01 22:07:55 +0300

    changes

---
+++ lesson-02/05.js
@@ -0,0 +1,10 @@
+function objectCopy() {
+  let passport = {
+    name: "Petr",
+    surname: "Petrov",
+  };
+  let copyPassport = { ...passport };
+  copyPassport.name = "Ivan";
+  console.log(passport);
+  console.log(copyPassport);
+}
