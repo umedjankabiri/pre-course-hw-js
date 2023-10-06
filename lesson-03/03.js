a799a68d730fb6ff3b018593d22e0fa157c41e96
UMEDJAN KABIRI <umedjan.kabiri@gmail.com>
2023-10-06 22:25:42 +0300

    changes

---
+++ lesson-03/03.js
@@ -0,0 +1,17 @@
+function sliceMethod() {
+  const fruits = [
+    "яблоко",
+    "банан",
+    "апельсин",
+    "манго",
+    "киви",
+    "ананас",
+    "виноград",
+    "груша",
+    "слива",
+    "персик",
+  ];
+  console.log(fruits);
+  console.log(fruits.slice(1, 7));
+}
+sliceMethod();
