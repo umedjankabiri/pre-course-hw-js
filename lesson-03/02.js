1698532605d28908ea138c943c3179a6cbc4f779
UMEDJAN KABIRI <umedjan.kabiri@gmail.com>
2023-10-06 21:58:24 +0300

    changes

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,6 +1,6 @@
 function emulationIndexOf() {
   const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
   if (words.indexOf("апельсин") !== -1) console.log("Ура! нашел");
-  else console.log("Придется поискать в другом магазине...");
+  else console.log("Придется поискать в другом магазине…");
 }
 emulationIndexOf();
