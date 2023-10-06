1a0423c2a3cc77e8994ab7307bc1e69b2b67752f
UMEDJAN KABIRI <umedjan.kabiri@gmail.com>
2023-10-06 15:32:28 +0300

    changes

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,9 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+function createColorString() {
+  return colors
+    .filter(
+      (value) => value == "черный" || value == "красный" || value == "желтый"
+    )
+    .join("-");
+}
+console.log(createColorString());
